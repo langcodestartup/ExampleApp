56ba4d4132bc3e462e4acc3ac6cc194930d3d91f
kimgooneya <kimgooneya@gmail.com>
2022-07-05 18:27:49 +0900

    null deserialize test

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine(2);
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+JsonObject jo = new()
+{
+    ["name"] = "suhyeon",
+    ["age"] = 34,
+};
+
+Person person = JsonSerializer.Deserialize<Person>(jo);
+
+Console.WriteLine(person.address);
+
+class Person
+{
+    public string name { get; set; }
+    public int age { get; set; }
+    public string address { get; set; }
+}
