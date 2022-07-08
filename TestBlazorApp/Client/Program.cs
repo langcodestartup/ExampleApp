0b478c01fcd1f74ae4d09bdec58b6b2eed250878
kimgooneya <kimgooneya@gmail.com>
2022-07-08 10:03:05 +0900

    클래스 라이브러리에서 자바스크립트 사용하기

--- TestBlazorApp/Client/Program.cs
+++ TestBlazorApp/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TestBlazorApp.Client;
+using TestBlazorApp.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +13,9 @@ builder.Services.AddHttpClient("TestBlazorApp.ServerAPI", client => client.BaseA
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("TestBlazorApp.ServerAPI"));
+builder.Services.AddScoped<ExampleJsInterop>();
+
+
 
 builder.Services.AddApiAuthorization();
 
