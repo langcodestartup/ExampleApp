7629190fdf1002a576f9ebffb4c67a1fde5aa184
kimgooneya <kimgooneya@gmail.com>
2022-07-20 20:25:15 +0900

    비동기 함수 예제

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,25 +1,26 @@
-﻿using System.ComponentModel;
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using Azure.Storage.Blobs;
-using Azure.Storage.Sas;
-using IronXL;
-
-IronXL.License.LicenseKey = "IRONXL.DEVTEAM.IRO220217.4447.58105.712022-80F0F23D30-PP5DIUOGV3IDZ-NENU5DWCHIZ2-KG46JTKM23RN-3C76N4HA6OKJ-BIPRPKWWOPVH-WKINMP-LWAIZOI7X3SIUA-LITE.SUB-HLGHJD.RENEW.SUPPORT.17.FEB.2023";
-string connectstring = "DefaultEndpointsProtocol=https;AccountName=spininvoicestorage;AccountKey=KgL7nes7uwlWxwFgY+lTnP7jplC4XODicr7jOXpr+BVemaJ6LAS7GTSuJGmU6XiuoI04nSPyXCyD+AStrxpU4g==;EndpointSuffix=core.windows.net";
-BlobServiceClient BlobServiceClient3 = new BlobServiceClient(connectstring);
-var BlobContainerClient3 = BlobServiceClient3.GetBlobContainerClient("testexcel");
-var blob = BlobContainerClient3.GetBlobClient("test.csv");
-var stream= await blob.OpenReadAsync();
-
-try
+﻿
+async Task MakeToast()
 {
-
-	WorkBook workbook1 = WorkBook.Load(stream);
-
+    Console.WriteLine("Make Toast");
+    await Task.Delay(4000);
+    Console.WriteLine("Toast Complete");
 }
-catch (Exception ex)
+
+async Task MakeEgg()
 {
-
-	throw;
+    Console.WriteLine("Make Egg");
+    await Task.Delay(2000);
+    Console.WriteLine("Egg Complete");
 }
+
+async Task MakeCoffe()
+{
+    Console.WriteLine("Make Coffee");
+    await Task.Delay(5000);
+    Console.WriteLine("Coffee Complete");
+}
+MakeCoffe().GetAwaiter();
+MakeEgg().GetAwaiter();
+MakeToast().GetAwaiter();
+
+Console.ReadKey();
