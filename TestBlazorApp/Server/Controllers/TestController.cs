693680d395c8c9722381d5890747d1df1284bebb
kimgooneya <kimgooneya@gmail.com>
2022-07-27 19:18:48 +0900

    bad request test

---
+++ TestBlazorApp/Server/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestBlazorApp.Server.Service;
+using TestBlazorApp.Shared;
+
+namespace TestBlazorApp.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        TestService testService;
+
+        public TestController(TestService testService)
+        {
+            this.testService = testService;
+        }
+
+        [HttpGet]
+        public async Task<string> TestCall()
+        {
+            testService.TestMethod();
+            return "hello";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCall(TestItem test)
+        {
+            return BadRequest("Error");
+        }
+    }
+}
